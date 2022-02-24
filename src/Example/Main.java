cebb08a19071bb79a317423b7d9ad01a04b2bd9f
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-02-24 19:32:24 +0100

    split all word by space

--- src/Example/Main.java
+++ src/Example/Main.java
@@ -5,18 +5,22 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 //update
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         String filename = System.getProperty("user.dir")+"\\input.txt";
-        ArrayList<String> records = new ArrayList<>();
+        ArrayList<ArrayList<String>> records = new ArrayList<>();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(filename));
             String line;
             while ((line = reader.readLine()) != null) {
-                records.add(line);
+                String[] currentLine = line.split(" ");
+                ArrayList<String> list = new ArrayList<>(Arrays.asList(currentLine));
+                records.add(list);
             }
             reader.close();
             System.out.println(records);
