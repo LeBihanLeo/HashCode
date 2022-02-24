acdf85891803bcdf0bb8d6312b15ac7cb43dbe0f
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-02-24 19:35:35 +0100

    create InputReader

---
+++ src/Example/InputReader.java
@@ -0,0 +1,28 @@
+package Example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class InputReader {
+    public static ArrayList<ArrayList<String>> readInput(){
+        String filename = System.getProperty("user.dir")+"\\input.txt";
+        ArrayList<ArrayList<String>> records = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] currentLine = line.split(" ");
+                ArrayList<String> list = new ArrayList<>(Arrays.asList(currentLine));
+                records.add(list);
+            }
+            reader.close();
+            return records;
+        } catch (Exception e) {
+            System.err.format("Exception occurred trying to read '%s'.", filename);
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- src/Example/Main.java
+++ src/Example/Main.java
@@ -12,22 +12,8 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        String filename = System.getProperty("user.dir")+"\\input.txt";
-        ArrayList<ArrayList<String>> records = new ArrayList<>();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] currentLine = line.split(" ");
-                ArrayList<String> list = new ArrayList<>(Arrays.asList(currentLine));
-                records.add(list);
-            }
-            reader.close();
-            System.out.println(records);
-        } catch (Exception e) {
-            System.err.format("Exception occurred trying to read '%s'.", filename);
-            e.printStackTrace();
-        }
+        ArrayList<ArrayList<String>> input = InputReader.readInput();
+        System.out.println(input);
     }
 }
 
