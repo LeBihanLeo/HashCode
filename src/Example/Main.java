47aee9df82e913feafe36d8bb3ee954dae4abb67
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-02-24 19:16:24 +0100

    Create reader

--- src/Example/Main.java
+++ src/Example/Main.java
@@ -1,7 +1,28 @@
 package Example;
 
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World");
+        String filename = "./input.txt";
+        ArrayList<String> records = new ArrayList<String>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                records.add(line);
+            }
+            reader.close();
+            System.out.println(records.toString());
+        } catch (Exception e) {
+            System.err.format("Exception occurred trying to read '%s'.", filename);
+            e.printStackTrace();
+        }
     }
 }
+
