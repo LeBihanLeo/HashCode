e32bec2b05b3630810b045814c21fe42fc15824d
Emile Derain <emilederain@gmail.com>
2022-02-24 19:20:44 +0100

    Start

--- src/Example/Job.java
+++ src/Example/Job.java
@@ -3,9 +3,15 @@ package Example;
 import java.util.List;
 
 public class Job {
+    String name;
     List<Skill> skillTaskList;
 
-    public Job(){
+    public Job(String name, List<Skill> skillList) {
+        this.name = name;
+        this.skillTaskList = skillTaskList;
+    }
 
+    public List<Skill> getJobs() {
+        return this.skillTaskList;
     }
 }
