8e58467ab899e0546da63a3a3c2fce46ac833c71
Emile Derain <emilederain@gmail.com>
2022-02-24 19:37:39 +0100

    Start

--- src/Example/Personnage.java
+++ src/Example/Personnage.java
@@ -5,11 +5,19 @@ import java.util.List;
 
 public class Personnage {
     String name;
+    int nbOfSkill;
     List<Skill> skillList = new ArrayList<>();
 
-    public Personnage(String name,List<Skill> skillList){
+    Job currentJob;
+
+    public Personnage(String name, List<Skill> skillList, int nbOfSkill) {
         this.name = name;
         this.skillList = skillList;
+        this.nbOfSkill = nbOfSkill;
+    }
+
+    public void setJob(Job job) {
+        this.currentJob = job;
     }
 
 }
